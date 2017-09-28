90b024195111984af4c88b6e56b823f2c4bd92fe
Juan <jfhernandeza8@gmail.com>
2017-09-28 18:01:27 -0300

    Started keypoint detection with Harris.

--- p3-163128-192744/src/utils/key_points.py
+++ p3-163128-192744/src/utils/key_points.py
@@ -4,3 +4,22 @@ from __future__ import print_function
 
 import cv2
 import numpy as np
+
+def decect_corners_harris(img, blocksize = 2, ksize = 3, k = 0.04, th = 0.1, margin = 5):
+
+	"""
+	Returns a list of  image coordinates whose Harris-detector score
+	is above th-percent of the maximum score of the image. The output
+	is prunned, so all the poins lie inside the specified margin.
+	"""
+
+	dst = cv2.cornerHarris(img, blocksize, ksize, k)
+	iind, jind = np.where(dst > th * dst.max())
+
+	sel = (iind >= margin) * (jind >= margin) * (iind < dst.shape[0] - margin) * (jind < dst.shape[1] - margin)
+
+	iind = np.reshape(iind[sel], (np.sum(sel),1))
+	jind = np.reshape(jind[sel], (np.sum(sel),1))
+
+	return np.concatenate((iind, jind), axis = 1)
+
--- p3-163128-192744/src/utils/klt.py
+++ p3-163128-192744/src/utils/klt.py
@@ -4,3 +4,45 @@ from __future__ import print_function
 
 import cv2
 import numpy as np
+import key_points
+from numpy.linalg import lstsq
+
+def get_motion_map(V, neigh = 15):
+
+	"""
+	Recieves a video represented by an np.array V[t][h][w]
+	
+	It does not work still!
+
+	dt = np.diff(V, axis = 0)
+	dx = np.diff(V, axis = 2)
+	dy = np.diff(V, axis = 1)
+
+	w_ran = range(-(neigh // 2), (neigh // 2) + (neigh % 2) )
+
+	for f in range(V.shape[0] - 1):
+
+		img = V[f]
+		X = key_points.decect_corners_harris(img)
+
+		A = np.zeros((neigh ** 2, 2))
+		b = np.zeros(neigh ** 2)
+
+		for p in X:
+
+			i = p[0]
+			j = p[1]
+
+			k = 0
+			for wi in w_ran:
+				for wj in w_ran:
+					A[k,0] =  dx[i + wi, j + wj]
+					A[k,1] =  dy[i + wi, j + wj]
+					b[k]   = -dt[i + wi, j + wj]
+					k++
+			
+
+		h = lstsq(A, b)
+
+"""
+
