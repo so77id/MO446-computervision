856d8acbec105e705a1b310b2cf2ad73de73869c
Icaro Cavalcante Dourado <icarocd@gmail.com>
2017-10-09 12:44:49 -0300

    P4: minor refactoring

--- p4-107396-163128-192744/src/main.py
+++ p4-107396-163128-192744/src/main.py
@@ -4,14 +4,14 @@ import util, descriptor, search
 def loadSamples():
     filePaths = util.getDirFilePaths('input')
     samples = []
-    for i,filePath in enumerate(filePaths):
+    for filePath in filePaths:
         name = filePath.rsplit('/')[1].rsplit('.')[0]
         label,index = name.rsplit('_')
-        samples.append(dict(id=i+1, name=name, path=filePath, label=label, index=index))
+        samples.append(dict(id=name, path=filePath, label=label, index=index))
     return samples
 
-def getSample(samples, name):
-    return next(s for s in samples if s['name'] == name)
+def getSample(samples, id):
+    return next(s for s in samples if s['id'] == id)
 
 if __name__ == '__main__':
     #CONFIGS:
@@ -19,14 +19,14 @@ if __name__ == '__main__':
     descriptorsCacheDir = 'output/descriptors'
     distanceFunctions = [distance.euclidean, distance.correlation]
     resultListSize = 3
-    querySampleNames = ['beach_2', 'boat_5', 'cherry_3', 'pond_2', 'stHelens_2', 'sunset1_2', 'sunset2_2']
+    queryIDs = ['beach_2', 'boat_5', 'cherry_3', 'pond_2', 'stHelens_2', 'sunset1_2', 'sunset2_2']
 
     samples = loadSamples()
 
     samplesDescriptors = descriptor.extractDescriptors(samples, descriptorsCacheDir, numSegments)
-    for querySampleName in querySampleNames:
-        querySample = getSample(samples, querySampleName)
+    for queryID in queryIDs:
+        querySample = getSample(samples, queryID)
         queryDescriptors = samplesDescriptors[querySample['id']]
         for distanceFunction in distanceFunctions:
             searchResult = search.query(queryDescriptors, distanceFunction, resultListSize, samplesDescriptors)
-            print('results for query '+querySampleName+' with distance '+ distanceFunction.__name__ +': ' + str(searchResult))
+            print('results for query '+queryID+' with distance '+ distanceFunction.__name__ +': ' + str(searchResult))
