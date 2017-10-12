329aa54e53c88f24e755cbad759f8f1e418a7c52
Icaro Cavalcante Dourado <icarocd@gmail.com>
2017-10-12 12:54:46 -0300

    search now computes and presents precision and average precision

--- p4-107396-163128-192744/src/main.py
+++ p4-107396-163128-192744/src/main.py
@@ -1,32 +1,43 @@
+import numpy as np
 from scipy.spatial import distance
+from collections import OrderedDict
 import util, descriptor, search
 
+#loads the samples, and returns a hash table <id,sample_dict>
 def loadSamples():
     filePaths = util.getDirFilePaths('input')
-    samples = []
+    samplesByID = OrderedDict()
     for filePath in filePaths:
         name = filePath.rsplit('/')[1].rsplit('.')[0]
         label,index = name.rsplit('_')
-        samples.append(dict(id=name, path=filePath, label=label, index=index))
-    return samples
-
-def getSample(samples, id):
-    return next(s for s in samples if s['id'] == id)
+        samplesByID[name] = (dict(id=name, path=filePath, label=label, index=index))
+    return samplesByID
 
 if __name__ == '__main__':
     #CONFIGS:
     numSegments = 5  # K for K-means
     descriptorsCacheDir = 'output/descriptors'
     distanceFunctions = [distance.euclidean, distance.correlation]
-    resultListSize = 3
+    resultSize = 3
     queryIDs = ['beach_2', 'boat_5', 'cherry_3', 'pond_2', 'stHelens_2', 'sunset1_2', 'sunset2_2']
 
-    samples = loadSamples()
+    samplesByID = loadSamples()
 
-    samplesDescriptors = descriptor.extractDescriptors(samples, descriptorsCacheDir, numSegments)
-    for queryID in queryIDs:
-        querySample = getSample(samples, queryID)
-        queryDescriptors = samplesDescriptors[querySample['id']]
-        for distanceFunction in distanceFunctions:
-            searchResult = search.query(queryDescriptors, distanceFunction, resultListSize, samplesDescriptors)
+    samplesDescriptors = descriptor.extractDescriptors(samplesByID.values(), descriptorsCacheDir, numSegments)
+    for distanceFunction in distanceFunctions:
+        precisionsAt, averagePrecisionsAt = [], []
+        for queryID in queryIDs:
+            queryLabel = samplesByID[queryID]['label']
+            queryDescriptors = samplesDescriptors[queryID]
+            searchResult = search.query(queryDescriptors, distanceFunction, resultSize, samplesDescriptors)
+            predictedLabels = [samplesByID[r['id']]['label'] for r in searchResult]
             print('results for query '+queryID+' with distance '+ distanceFunction.__name__ +': ' + str(searchResult))
+            #computes quality:
+            precisionAt, averagePrecisionAt = search.precision(predictedLabels, queryLabel), search.averagePrecision(predictedLabels, queryLabel)
+            precisionsAt.append(precisionAt)
+            averagePrecisionsAt.append(averagePrecisionAt)
+            print('\tP@%d: %f, AP@%d: %f' % (resultSize, precisionAt, resultSize, averagePrecisionAt))
+        meanP, stdP = np.mean(precisionsAt), np.std(precisionsAt)
+        meanAP, stdAP = np.mean(averagePrecisionsAt), np.std(averagePrecisionsAt)
+        print('# results for distance %s: P@%d: %.2f +- %.2f, AP@%d: %.2f +- %.2f' %
+            (distanceFunction.__name__, len(queryIDs), meanP*100, stdP*100, len(queryIDs), meanAP*100, stdAP*100))
--- p4-107396-163128-192744/src/search.py
+++ p4-107396-163128-192744/src/search.py
@@ -3,13 +3,17 @@ def query(queryDescriptors, distanceFunction, resultListSize, samplesDescriptors
     results = []
     for trainId,trainSampleDescriptors in samplesDescriptors.items():
         d = dist(queryDescriptors, trainSampleDescriptors, distanceFunction)
-        results.append((trainId,d))
-    results.sort(key=lambda pair: pair[1], reverse=False)
+        results.append(dict(id=trainId,distance=d))
+    results.sort(key=lambda pair: pair['distance'], reverse=False)
     if len(results) > resultListSize:
         results = results[:resultListSize]
     return results
 
 def dist(queryDescriptors, trainSampleDescriptors, distanceFunction):
+    #TODO mocking distances for now, while descriptors are not done yet
+    import random
+    return round(random.uniform(0.1,1.0),2)
+
     d = 0
     for queryDescriptor in queryDescriptors:
         dMin = 999999
@@ -19,3 +23,16 @@ def dist(queryDescriptors, trainSampleDescriptors, distanceFunction):
                 dMin = d_
         d += dMin
     return d
+
+def precision(predictedClasses, groundTruthClass):
+    return predictedClasses.count(groundTruthClass) / len(predictedClasses)
+
+#calculates AP@K, assuming there are more possible correct results than those obtained
+def averagePrecision(predictedClasses, groundTruthClass):
+    score = 0.0
+    hits = 0.0
+    for i,p in enumerate(predictedClasses):
+        if p == groundTruthClass:
+            hits += 1.0
+            score += hits / (i + 1.0)
+    return score / len(predictedClasses)
